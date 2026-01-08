f22bd558e72c83043fc09eeaa5dc3de0149cfc11
runing9318-ship-it <runing9318@gmail.com>
2026-01-09 01:50:21 +0800

    Add files via upload

---
+++ Untitled-3.js
@@ -0,0 +1,15 @@
+// 技能條動畫
+window.addEventListener("load", () => {
+    const progresses = document.querySelectorAll(".progress");
+    progresses.forEach(bar => {
+        const percent = bar.getAttribute("data-percent");
+        bar.style.width = percent + "%";
+    });
+});
+
+// 聯絡按鈕互動
+document.getElementById("contactBtn").addEventListener("click", () => {
+    alert;
+git remote add origin https://github.com/runing9318-ship-it/113051137-.git
+ git branch -M main 
+git push -u origin main
